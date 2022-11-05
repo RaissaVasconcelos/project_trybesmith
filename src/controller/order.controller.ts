ba5800b42f81766eac92fd76bf4c1498f31d5683
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-04 21:47:35 -0300

    Realizado correção requisito 4.

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import OrderService from '../service/order.service';
 
-export default class ProductController {
+export default class OrderController {
   constructor(private orderService = new OrderService()) {}
 
   async getAll(req: Request, res: Response): Promise<void> {
